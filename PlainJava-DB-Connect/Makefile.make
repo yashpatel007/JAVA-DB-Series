fd8a43b322accf4be2c0d1288a9907c64cff9a76
unknown <patelyash1311@gmail.com>
2020-04-26 15:48:56 -0400

    lag fix

---
+++ PlainJava-DB-Connect/Makefile.make
@@ -0,0 +1,2 @@
+all:
+	mvn exec:java -Dexec.mainClass=com.yashpatel.plainjava.db.connect.Main
--- PlainJava-DB-Connect/src/main/java/com/yashpatel/plainjava/db/connect/Main.java
+++ PlainJava-DB-Connect/src/main/java/com/yashpatel/plainjava/db/connect/Main.java
@@ -20,13 +20,11 @@ public class Main extends javax.swing.JFrame {
      * Creates new form Main
      */
     
-    static QuerySearcher querySearcher;
+    static QuerySearcher querySearcher = new QuerySearcher();
     //= new QuerySearcher();;
     public Main() {
         initComponents();
-        //DBConnector dBConnector = new DBConnector();
-        //this.querySearcher= 
-        
+        //DBConnector dBConnector = new DBConnector();        
     }
 
     /**
@@ -55,7 +53,6 @@ public class Main extends javax.swing.JFrame {
         updateDescription = new javax.swing.JTextArea();
         jLabel4 = new javax.swing.JLabel();
         addToDB = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         resultText = new javax.swing.JTextArea();
@@ -180,13 +177,6 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("connect");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -202,20 +192,13 @@ public class Main extends javax.swing.JFrame {
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addGap(0, 107, Short.MAX_VALUE)
                                 .addComponent(getUnCompleted, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(getAll, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(53, 53, 53))
                             .addComponent(jScrollPane1)
                             .addComponent(updateTitle, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addGap(0, 0, Short.MAX_VALUE))
-                            .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 266, Short.MAX_VALUE)
-                                .addComponent(jButton1))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(inputQuery)
@@ -223,7 +206,12 @@ public class Main extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(getByTitle, javax.swing.GroupLayout.DEFAULT_SIZE, 91, Short.MAX_VALUE)
-                                    .addComponent(searchQuery, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                                    .addComponent(searchQuery, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(0, 0, Short.MAX_VALUE)))
                         .addContainerGap())))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -264,9 +252,7 @@ public class Main extends javax.swing.JFrame {
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(getUnCompleted, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(getAll, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(5, 5, 5))
+                .addGap(31, 31, 31))
             .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                     .addContainerGap(433, Short.MAX_VALUE)
@@ -369,28 +355,15 @@ public class Main extends javax.swing.JFrame {
         
     }//GEN-LAST:event_getUnCompletedActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-       resultText.append("Please wait trying to make connection, May take a while");
-       jButton1.setEnabled(false);
-       querySearcher = new QuerySearcher();
-       if(querySearcher.connection != null){
-           resultText.setText("connection sucessfull");
-       }
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
-
     public void setResultText(ArrayList<Todo> res){
         resultText.setText("");
         if(res.size()==0){
             resultText.setText("No records found") ;
         }
         
-        
         for(Todo item : res){
             resultText.append(item.toString()+"\n");
         }
-        
     }
     
     /**
@@ -428,8 +401,6 @@ public class Main extends javax.swing.JFrame {
         });
     }
     
-    
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addToDB;
     private javax.swing.JButton getAll;
@@ -438,7 +409,6 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JButton getUnCompleted;
     private javax.swing.JTextField inputQuery;
     private javax.swing.JTextField inputTitle;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -454,5 +424,4 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JTextField updateTitle;
     // End of variables declaration//GEN-END:variables
 
-    
 }
